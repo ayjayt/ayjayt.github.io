bc71f9bf5fa897496abf97990f2fb5d2ba8c2aeb
Andrew Pikul <ajpikul@gmail.com>
2020-04-16 01:03:20 -0400

    Add promise to main to render graph

--- js/main.js
+++ js/main.js
@@ -21,14 +21,22 @@ function iflog(message) {
 // Execute all javascript when window is ready here
 window.addEventListener("load", (event) => {
 	// STATE: 1, Window Loaded
-	iflog("window.load(): Window Loaded")
+	iflog("window.load(): Window Loaded");
 
 	// NOTE: we won't use Promise.all because we want each promise to be evaluated individually for success or failure- not fail on first.
-	DATAURLS.map(retrieveData).forEach((promise) => {
-		// Note: can't call class method directly because promise strips it from it's class instance >:O
+	var remoteDataProm = DATAURLS.map(retrieveData);
+
+	remoteDataProm.forEach((promise) => {
+		// Note: can't pass the class method directly because promise strips it from it's class instance >:O
+		// So it's passed as a lambda
 		promise.then(raw => data.readCSV(raw)).catch(address => { 
 			// STATE: Error, address failed
 			iflog("window.load(): " + address + " did not resolve properly.") 
 		})
 	})
+
+	Promise.all(remoteDataProm).finally( () => {
+		data.writeMajorColumns(); 
+		data.writeBarGraph(sampleFiltered);
+	});
 })
